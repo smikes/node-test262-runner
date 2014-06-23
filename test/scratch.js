26df825d264acb8bbc2f13647aba20b254aaeb79
Sam Mikes <smikes@cubane.com>
2014-06-23 15:15:12 -0600

    meta: initial working versions

---
+++ test/scratch.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe("test runner", function () {
+    it("should load up the test runner and run a test", function (done) {
+	assert.ok(true);
+	
+	done();
+    });
+});
